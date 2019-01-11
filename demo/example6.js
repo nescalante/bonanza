b9ee57cfc3cc5bb1a43461240a32d0af8b408b68
Nicolas Escalante <nlante@gmail.com>
2019-01-11 11:38:34 -0300

    fix demo playground

---
+++ demo/example6.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var bonanza = require('../src');
+var util = require('../src/util.js');
+var list = require('./list.json');
+
+module.exports = function () {
+  bonanza(document.querySelector('#example6'), { templates: { item: function (obj) { return obj.firstName + ' ' + obj.lastName; }, isDisabled: function (obj) { return obj.isDisabled; } } }, request);
+};
+
+function request(query, done) {
+  console.info('Loading using query: ', query);
+  setTimeout(function () {
+    var items = list
+      .filter(function (item) {
+        return util.queryRegExp(query.search).test(item.firstName + ' ' + item.lastName);
+      })
+      .slice(query.offset, query.offset + query.limit);
+
+    done(null,
+     items);
+  }, 300);
+}
